f4594f4966ff0eb3bd2d69bc12e51293e40c1a77
Mikhail <mzhigulin@gmail.com>
2022-11-18 22:29:13 +0700

    INCLUDED: SwiftLint config and shell script
    This commit goes with cleaned source code by SwiftLint.
    
    SwiftLint must be installed on the machine running macOS High Sierra or higher.
    SwiftLint in Use: 0.31.0: Busy Laundromat installed via .pkg.
    
    ADDED: SwiftLint config file.
    
    ADDED: SwiftLint shell script.
    
    CLEANED: Source code.

--- XcodeTemplateProject/BusinessContent/MainWindowController.swift
+++ XcodeTemplateProject/BusinessContent/MainWindowController.swift
@@ -15,7 +15,5 @@ class MainWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-    
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 }
