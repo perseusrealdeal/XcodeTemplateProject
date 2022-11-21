f875b8dc2c0ffa3dfc2a8cf4cb6d6445f2fb19a8
Mikhail <mzhigulin@gmail.com>
2022-11-21 22:41:53 +0700

    UPDATED: license source marks
    This commit goes with updated license source code marks.

--- XcodeTemplateProjectTests/SettingsBundleTests.swift
+++ XcodeTemplateProjectTests/SettingsBundleTests.swift
@@ -5,6 +5,7 @@
 //  Created by Mikhail Zhigulin in 7531.
 //
 //  Copyright © 7531 Mikhail Zhigulin of Novosibirsk.
+//
 //  Licensed under the special license. See LICENSE file.
 //  All rights reserved.
 //
